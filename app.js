a107f67e47acf03e7f672b043af9b6b048ed44c1
dawimuhammad <muhamadhaddawi@gmail.com>
2018-07-01 00:56:10 +0700

    Release 5 Add route student and teacher with good looking interfaces

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const app = require('express')()
 const routes = require('./routes')
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.set('view engine', 'ejs')
 
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,28 @@ const routes = require('express').Router()
 const path = require('path')
 const ejs = require('ejs')
 
-// routes.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname+'/index.html'));
-// })
-
 routes.get('/', (req, res) => {
     res.render('homepage')
+})
+
+routes.get('/student', (req, res) => {
+    res.render('student')
+})
+
+routes.post('/student', (req, res) => {
+    let studentData = req.body
+    res.render('student-show.ejs', {
+        firstname : `${studentData.inputFirstname}`,
+        lastname : `${studentData.inputLastname}`,
+        gender : `${studentData.inputGender}`,
+        birthday : `${studentData.inputDOB}`,
+        email : `${studentData.inputEmail}`,
+        phone : `${studentData.inputPhone}`
+    })
+})
+
+routes.get('/teacher', (req, res) => {
+    res.render('teacher')
 })
 
 module.exports = routes
