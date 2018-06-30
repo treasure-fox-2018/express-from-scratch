84f96aeb062a44d8212ae31315273b6c6023125e
dawimuhammad <muhamadhaddawi@gmail.com>
2018-06-30 10:27:50 +0700

    Release 4 - Add routes /

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 const app = require('express')()
 const routes = require('./routes')
+const ejs = require('ejs')
+
+app.set('view engine', 'ejs')
 
 app.use('/', routes)
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 'use strict'
 
 const routes = require('express').Router()
+const path = require('path')
+const ejs = require('ejs')
 
-routes.get('/', function (req, res) {
-    res.send("COME ON YOU GUNNERS !")
+// routes.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname+'/index.html'));
+// })
+
+routes.get('/', (req, res) => {
+    res.render('homepage')
 })
 
 module.exports = routes
