4117e3d2e8b4c0f3d8d3c9be960f6c65b2a97be6
Your Name <fahrisyany@gmail.com>
2018-06-30 18:59:53 +0700

    add a cool index page for testing

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,26 @@ let path = require('path')
 
 let bp = (bodyparse.urlencoded({ extended: false }));
 
+// routes.get("/", (req, res) => {
+//     res.send('i love hacktiv 8')
 
-
-
-
+// })
 
 routes.get('/', (req, res) => {
+    res.render('index', { title: 'Home' })
+})
+
+
+routes.get('/student', (req, res) => {
     res.render('student.ejs', { Name: null, Email: null, About: null });
     console.log('port 3000 ONLINE')
 });
 
 routes.post('/student', bp, function (req, res) {
-    res.render('studentData.ejs', {data: req.body})
+    res.render('studentData.ejs', { data: req.body })
 })
 
-routes.get('/teacher',(req,res)=>{
+routes.get('/teacher', (req, res) => {
     res.render('teacher.ejs');
 });
 
