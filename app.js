692180e4036b46d2238af030ddc30f094e61a0bc
davidyunus <35801752+davidyunus@users.noreply.github.com>
2018-07-02 12:02:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
 const bodyParser = require('body-parser')
-const home = require('./routes/index.js')
+const index = require('./routes/index.js')
 const student = require('./routes/student.js')
 const teacher = require('./routes/teacher.js')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.set('view engine', 'ejs')
-app.use('/', home)
-app.use(student, teacher)
+app.use('/', index)
+app.use('/student', student)
+app.use('/teacher', teacher)
+
 
 app.listen(3000, function() {
-    console.log('Listening on port 3000')
+    console.log('Online')
 });
